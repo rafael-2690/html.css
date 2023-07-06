6805fa09f3f7eca4879c5170755eb7473b0433bd
Rafael Pinto Pereira <122949045+rafael-2690@users.noreply.github.com>
2023-07-06 10:21:37 -0300

    .

---
+++ Testes/Teste1/script.js
@@ -0,0 +1,15 @@
+function sim(){
+    alert("Você aceitou namorar comigo! :)")
+}
+
+function desviar(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = posisao(10, 90);
+    btn,style.left = posisao (10, 90);
+    console.log ("Opa. desviei...");
+}
+
+function posisao(min, max){
+    return (Math.random() * (max - min) + min) + "%";
+}
